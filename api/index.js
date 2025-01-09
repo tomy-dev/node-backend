c747c651c2200212e61029707e00ba0a436a77de
Oscar Jurado <andres.ortiz900106@gmail.com>
2025-01-08 21:55:22 -0500

    deploy

--- api/index.js
+++ api/index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const cors = require('cors');
-const port = 8080;
+const port = process.env.PORT || 8080;
 const app = express();
 app.use(express.json());
 const witheList = ['http://localhost:8080', 'https://myapp.co'];
 const corsOptions = {
     origin: function (origin, callback) {
-        if (witheList.includes(origin)) {
+        if (witheList.includes(origin) || !origin) {
             callback(null, true);
         } else {
             callback(new Error('Not allowed by CORS'));
